6c22a38e94ed94d047bad3d9b004c9b073dad416
Ignat Zakrevsky <iezakrevsky@gmail.com>
2015-11-26 02:26:59 +0300

    make app sup

--- lib/subscriber.ex
+++ lib/subscriber.ex
@@ -1,2 +1,13 @@
 defmodule Subscriber do
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec
+
+    children = [
+    ]
+
+    opts = [strategy: :one_for_one, name: Subscriber.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
 end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,10 @@ defmodule Subscriber.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: (Mix.env == :dev && [:exsync] || []) ++ [:logger, :maru]]
+    [applications:
+      (Mix.env == :dev && [:exsync] || []) ++
+      [:logger, :maru],
+      mod: {Subscriber, []}]
   end
 
   # Dependencies can be Hex packages:
@@ -27,8 +30,8 @@ defmodule Subscriber.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [ { :maru,       "~> 0.8"},
-      { :exsync,     "~> 0.1", only: :dev },
+    [ { :maru,         "~> 0.8" },
+      { :exsync,       "~> 0.1", only: :dev }
     ]
   end
 end
