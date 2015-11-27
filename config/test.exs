a7a3d7df5ee2fd14621b3f9cdd5cf76a5e0a42ce
Ignat Zakrevsky <iezakrevsky@gmail.com>
2015-11-28 01:57:38 +0300

    move main file to properly path

---
+++ config/test.exs
@@ -0,0 +1,16 @@
+use Mix.Config
+
+config :subscriber_app, SubscriberApp.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  database: "subscriber_test",
+  username: "postgres",
+  password: "root",
+  hostname: "localhost"
+
+config :exredis,
+  host: "127.0.0.1",
+  port: 6379,
+  password: "",
+  db: 0,
+  reconnect: :no_reconnect,
+  max_queue: :infinity
---
+++ test/models/subscriber_test.exs
@@ -0,0 +1,24 @@
+defmodule SubscriberApp.SubscriberTest do
+  use ExUnit.Case
+
+  alias SubscriberApp.Subscriber
+
+  @valid_attrs %{email: "a@b.com"}
+  @invalid_email_attrs %{email: "aaa"}
+  @invalid_empty_attrs %{}
+
+  test "changeset with valid attrs" do
+    changeset = Subscriber.changeset(%Subscriber{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid email" do
+    changeset = Subscriber.changeset(%Subscriber{}, @invalid_email_attrs)
+    refute changeset.valid?
+  end
+
+  test "empty changeset" do
+    changeset = Subscriber.changeset(%Subscriber{}, @invalid_email_attrs)
+    refute changeset.valid?
+  end
+end
